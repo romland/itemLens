c7be5717e8bbd421ae6f708051383ffb99f64374
Joakim Romland <romland@gmail.com>
2024-02-14 10:01:36 +0100

    Ditched unused code

--- src/routes/(items)/add/+page.svelte
+++ src/routes/(items)/add/+page.svelte
@@ -6,11 +6,10 @@
     import Title from "$lib/components/Title.svelte";
 
     export let form: ActionData;
-
 </script>
 
 <svelte:head>
-    <Title>Add New Item</Title>
+    <Title>Add new item</Title>
 </svelte:head>
 
 {#if form?.error}
@@ -21,17 +20,16 @@
     <div class="mb-3">
         <input type="text" name="title" value="Default name" placeholder="Title" class="input input-bordered w-full">
     </div>
-    {#if !cameraUsed}
-        <div class="mb-3">
-            <!--input id="eltFileInput" type="file" name="file" accept="image/*" class="file-input w-full"-->
-            <input id="eltFileInput" type="file" name="file" accept="image/*" capture="environment" class="file-input w-full">
-        </div>
-    {:else}
-        <canvas id="eltPreviewCanvas" style="width:{eltCanvas.width * 0.25}px; height:{eltCanvas.height * 0.25}px;" />
-    {/if}
+
+    <div class="mb-3">
+        <!--input id="eltFileInput" type="file" name="file" accept="image/*" class="file-input w-full"-->
+        <input id="eltFileInput" type="file" name="file" accept="image/*" capture="environment" class="file-input w-full">
+    </div>
+
     <div class="mb-3">
         <textarea name="content" rows="5" placeholder="Content" class="textarea textarea-bordered w-full"></textarea>
     </div>
+
     <div class="mb-3">
         <input type="text" name="tagcsv" placeholder="Tags" class="input input-bordered w-full">
         <div class="mt-1 text-gray-400 text-xs">
@@ -40,5 +38,4 @@
     </div>
 
     <button type="submit" class="btn btn-primary">Save</button>
-
 </form>
