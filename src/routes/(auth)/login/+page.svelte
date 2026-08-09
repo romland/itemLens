75996cbf60ca0b508d95092542a8fa57b4f8a97d
Joakim Romland <romland@gmail.com>
2026-08-09 21:58:18 +0200

    Visual feedback on authentication

--- src/routes/(auth)/login/+page.svelte
+++ src/routes/(auth)/login/+page.svelte
@@ -5,6 +5,8 @@
 
     export let form: ActionData;
 
+	let isLoggingIn = false;
+
     import pageTitle from '$lib/stores';
     pageTitle.set("Log in");
 </script>
@@ -13,7 +15,13 @@
     <Alert>{@html form?.message}</Alert>
 {/if}
 
-<form method="post" use:enhance>
+<form method="post" use:enhance={() => {
+    isLoggingIn = true;
+    return async ({ update }) => {
+        await update();
+        isLoggingIn = false;
+    };
+}}>
 	<div class="mb-3">
 		<input type="text" name="username" placeholder="Username" class="input input-bordered w-full">
 	</div>
@@ -21,7 +29,10 @@
 		<input type="password" name="password" placeholder="Password"  class="input input-bordered w-full">
 	</div>
 	<div class="inline-flex items-center gap-3">
-		<button type="submit" class="btn btn-primary">Sign In</button>
+		<button type="submit" class="btn btn-primary" disabled={isLoggingIn}>
+			{#if isLoggingIn}<span class="loading loading-spinner loading-sm"></span>{/if}
+			Sign In
+		</button>
 		<a href="/register">Register</a>
 	</div>
 </form>
