8806eadf53aeb22830db6e52033d3bab4117c568
Joakim Romland <romland@gmail.com>
2026-08-12 11:09:26 +0200

    Shoo

--- src/lib/components/ReloadPrompt.svelte
+++ src/lib/components/ReloadPrompt.svelte
@@ -39,7 +39,7 @@
 			{/if}
 		-->
             <span>
-                New content available, click reload to update.
+                New content available.
             </span>
 		</div>
 		{#if $needRefresh}
